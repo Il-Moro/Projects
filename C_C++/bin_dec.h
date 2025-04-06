b8e3fe729738f5dd656a5f7c6c2a91b40f6dfeb3
Il-moro <filmor04@gmail.com>
2025-04-06 11:44:43 +0200

    6-aprile-2025

--- C_C++/bin_dec.h
+++ C_C++/bin_dec.h
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bin_dec();
+void bin_dec(){
+    int esp, mantissa; // esponente e mantissa nel numero macchina
+    int decisione = 1;
+    int num, lenght;
+    int * arr; 
+    printf("Type number of cifre for exp: "); 
+    scanf("%d", &esp);
+    printf("Type number of mantissa: ");
+    scanf("%d", &mantissa);
 
+    
+        while (decisione == 1){
+            printf("Type binary number: ");
+            scanf("%d", &num);
+            while (num > 0){
+                print("hello world");
+            }
+        }
+}
